71f07b952daf094e99420301a18d6af7b7f6da3b
Josep Monclus <josep.monclus@gmail.com>
2021-10-13 13:37:39 +0200

    Problem 4 solved!

--- problem0004.java
+++ problem0004.java
@@ -1,12 +1,55 @@
 /**
  * Problem url: https://projecteuler.net/problem=4
  * 
- * @author Josep Monclús
+ * @author Josep Monclus
  */
 public class problem0004 {
     public static void main(String[] args) {
-        int max = 99;
-
+        int max = 999;
         int maxPalindrome = 0;
+
+        for(int i = 1; i <= max; i++) {
+            for(int j = 1; j <= max; j++) {
+                int value = i * j;
+
+                if(isPalindrome(value) && value > maxPalindrome) {
+                    maxPalindrome = value;
+                }
+            }
+        }
+
+        System.out.println("MAX PALINDROME: " + maxPalindrome);
+    }
+
+    private static boolean isPalindrome(int value) {
+        String sValue = String.valueOf(value);
+        String s1 = "", s2 = "";
+
+        if(sValue.length() % 2 == 0) { // Even length
+            s1 = sValue.substring(0, sValue.length() / 2);
+            s2 = sValue.substring(sValue.length() / 2, sValue.length());
+        } else { // Odd length
+            s1 = sValue.substring(0, sValue.length() / 2 + 1);
+            s2 = sValue.substring(sValue.length() / 2, sValue.length());
+        }
+
+        if(s1.equals(reverse(s2))){
+            System.out.println(sValue + " - " + s1 + " - " + s2);
+
+            return true;
+        }
+
+        return false;
+    }
+
+
+    private static String reverse(String s) {
+        StringBuilder reversed = new StringBuilder();
+
+        for(int i = s.length() - 1; i >= 0; i--){
+            reversed.append(s.charAt(i));
+        }
+
+        return reversed.toString();
     }
 }
