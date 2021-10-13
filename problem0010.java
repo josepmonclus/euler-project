fd83b5105a889c3bc28aac319a1537452b034b2e
Josep Monclus <josep.monclus@gmail.com>
2021-10-13 15:05:59 +0200

    Problem 10 solved!

---
+++ problem0010.java
@@ -0,0 +1,36 @@
+import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.List;
+
+public class problem0010 {
+
+    private static List<Integer> primes = new ArrayList<>();
+    public static void main(String[] args) {
+        int primesBelow = 2000000;
+
+        BigInteger sum = BigInteger.ZERO;
+
+        for(int i = 2; i < primesBelow; i++) {
+            if(isPrime(i)) {
+                primes.add(i);
+
+                sum = sum.add(BigInteger.valueOf(i));
+            }
+        }
+
+        System.out.println("Sum: " + sum);
+    }
+
+    private static boolean isPrime(int i) {
+        boolean isPrime = true;
+        
+        for(int prime : primes) {
+            if(i % prime == 0) {
+                isPrime = false;
+                break;
+            }
+        }
+
+        return isPrime;
+    }
+}
