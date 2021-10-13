e5115cf57363f087627b25108b3dea1a800180d1
Josep Monclus <josep.monclus@gmail.com>
2021-10-13 14:00:14 +0200

    Problem 6 solved!

---
+++ problem0006.java
@@ -0,0 +1,35 @@
+public class problem0006 {
+    
+    public static void main(String[] args) {
+        int maxValue = 100;
+
+        int sumOfSquares = sumOfSquares(maxValue);
+        int squareOfSums = squareOfSums(maxValue);
+
+        int diff = squareOfSums - sumOfSquares;
+
+        System.out.println("Difference: " + diff);
+    }
+
+    private static int sumOfSquares(int maxValue) {
+        int sumOfSquares = 0;
+
+        for(int i = 1; i <= maxValue; i++) {
+            sumOfSquares += i * i;
+        }
+
+        return sumOfSquares;
+    }
+
+    private static int squareOfSums(int maxValue) {
+        int squareOfSums = 0;
+
+        for(int i = 1; i <= maxValue; i++) {
+            squareOfSums += i;
+        }
+
+        squareOfSums = squareOfSums * squareOfSums;
+
+        return squareOfSums;
+    }
+}
