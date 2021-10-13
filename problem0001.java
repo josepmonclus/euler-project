60c6aedddf2b7338c00ec9443ea1c6f29e874936
Josep Monclus <josep.monclus@gmail.com>
2021-10-13 11:02:37 +0200

    Cambios de nombres

---
+++ problem0001.java
@@ -0,0 +1,17 @@
+public class problem0001 {
+    public static void main(String[] args) {
+        
+        int n = 1000;
+        int total = 0;
+
+        while (n > 0) {
+            n--;
+
+            if(n % 3 == 0 || n % 5 == 0) {
+                total += n;
+            }
+        }
+
+        System.out.println(total);
+    }
+}
---
+++ problem0003.java
@@ -0,0 +1,5 @@
+public class problem0003 {
+    public static void main(String[] args) {
+        
+    }
+}
