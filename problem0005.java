53d20a11f7c0cd11f0f84d34ae2e876dab106349
Josep Monclus <josep.monclus@gmail.com>
2021-10-13 13:46:33 +0200

    Problem 5 solved!

--- problem0005.java
+++ problem0005.java
@@ -1,10 +1,29 @@
 /**
  * Problem url: https://projecteuler.net/problem=5
  * 
- * @author Josep Monclús
+ * @author Josep Monclus
  */
 public class problem0005 {
     public static void main(String[] args) {
-        
+        int maxMult = 20;
+
+        int value = 1;
+
+        while(true) {
+            boolean found = true;
+
+            for(int i = 1; i <= maxMult; i++) {
+                if(value % i != 0) {
+                    found = false;
+                    break;
+                }
+            }
+
+            if(found) break;
+
+            value++;
+        }
+
+        System.out.println("Value: " + value);
     }
 }
