330c5f3f8c94fc6db3bfed4ded659bdd5251f60b
Josep Monclus <monclusj@aia.es>
2022-04-11 09:50:48 +0200

    Problem 25 solved!!

---
+++ com/jmonclus/problems/Problem0025.java
@@ -0,0 +1,26 @@
+package com.jmonclus.problems;
+
+import java.math.BigDecimal;
+
+public class Problem0025 {
+    public static void main(String[] args) {
+        int nDigits = 1000;
+
+        BigDecimal n1 = new BigDecimal(1);
+        BigDecimal n2 = new BigDecimal(1);
+        int count = 2;
+        
+        BigDecimal aux;
+
+        while (n2.toString().toCharArray().length != nDigits) {
+            aux = n1.add(n2);
+            
+            n1 = n2;
+            n2 = aux;
+
+            count++;
+        }
+
+        System.out.println("First Fibonacci term with " + nDigits + " digits is: " + count);
+    }
+}
