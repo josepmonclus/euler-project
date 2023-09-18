854d485f5e82cd63967b61e1b5bedccd5b2186a0
Josep Monclus <monclusj@aia.es>
2023-09-18 16:20:29 +0200

    Problem 20 solved in python

---
+++ python/problem_0020.py
@@ -0,0 +1,14 @@
+
+# Problem url: https://projecteuler.net/problem=19
+
+n = 100
+
+factorial = 1
+for i in range(n, 0, -1):
+    factorial *= i
+
+sum = 0
+for digit in str(factorial):
+    sum += int(digit)
+
+print(str(n) + '! is:', factorial, 'and the sum of digits is:', sum)
